996a3f0a5c66881ad995a3baf453555a0d47a019
feng <292280685@qq.com>
2019-03-11 21:40:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/boxuegu/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.boxuegu.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.boxuegu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
